4f0bcc458ed278d8263c5ead440743bec9308e71
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-12 11:43:49 +0530

    Fixed bug of printing id of student

--- ruby_db/student.rb
+++ ruby_db/student.rb
@@ -4,5 +4,6 @@ database = Sequel.sqlite('database.sqlite3')
 #database.run "INSERT INTO students VALUES(1,'Manish');"
 results = database.fetch "SELECT * FROM students;"
 results.each do |tuple|
-	puts #{tuple[:id]}+tuple[:name]
+	puts "#{tuple[:s_id]}  " + tuple[:name]
 end
+ 
