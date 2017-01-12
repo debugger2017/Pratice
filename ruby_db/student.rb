9fe7a58d7bd79c5a984bc6c21bc350e61ca1492d
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-12 11:13:51 +0530

    Added code for insert,select and fetch

--- ruby_db/student.rb
+++ ruby_db/student.rb
@@ -1,6 +1,10 @@
 require 'sqlite3'
 require 'sequel'
 database = Sequel.sqlite('database.sqlite3')
-database.run "CREATE TABLE students (s_id integer primary key autoincrement , name varchar(255))"
+#database.run "CREATE TABLE students (s_id integer primary key autoincrement , name varchar(255))"
 #database.run "DROP TABLE students4"
-database.run "INSERT INTO students VALUES(1,'Kartik')"
+#database.run "INSERT INTO students VALUES(3,'Mrunal')"
+#database.run "SELECT * FROM students;"
+results = database.fetch "SELECT * FROM students;"
+#results.to_a
+puts results
