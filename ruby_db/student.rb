199c52837112b506fe3dfbff05fa4abf23c2cc93
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-12 11:38:27 +0530

    Fixed bug of fetching data

--- ruby_db/student.rb
+++ ruby_db/student.rb
@@ -1,10 +1,8 @@
 require 'sqlite3'
 require 'sequel'
 database = Sequel.sqlite('database.sqlite3')
-#database.run "CREATE TABLE students (s_id integer primary key autoincrement , name varchar(255))"
-#database.run "DROP TABLE students4"
-#database.run "INSERT INTO students VALUES(3,'Mrunal')"
-#database.run "SELECT * FROM students;"
+#database.run "INSERT INTO students VALUES(1,'Manish');"
 results = database.fetch "SELECT * FROM students;"
-#results.to_a
-puts results
+results.each do |tuple|
+	puts #{tuple[:id]}+tuple[:name]
+end
